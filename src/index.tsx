81a3d51559f487db556e1fdfd2ce1cdb6a30e13e
davidgu13 <davidgu1312@gmail.com>
2023-04-07 17:07:16 +0300

    added eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+// This project was initialized with the instructions in https://dev.to/alekseiberezkin/setting-up-react-typescript-app-without-create-react-app-oph
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
