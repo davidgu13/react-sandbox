2c8a7879010c0997b108dfa2d637dcbf02ddc3a3
davidgu13 <davidgu1312@gmail.com>
2023-04-08 10:57:05 +0300

    fixed linter, incl. import order; added App component

---
+++ src/App/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./app.css";
+
+const App = (): JSX.Element => {
+    return (
+        <>
+            <h1>Hellooooo00 React!</h1>
+        </>
+    );
+};
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 // This project was initialized with the instructions in https://dev.to/alekseiberezkin/setting-up-react-typescript-app-without-create-react-app-oph
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import App from "./App";
 
 const container = document.getElementById("app-root")!;
 const root = createRoot(container);
-root.render(<h1>Hello React!</h1>);
+root.render(<App />);
